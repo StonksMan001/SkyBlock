b5aaecc87bcfa786004e8d85464d9df469bc3d65
StonksMan001 <toth.abel.2007@gmail.com>
2023-05-13 00:08:17 +0200

    Added: Classified

---
+++ src/main/java/net/me/skyblock/block/custom/BlueVigilantGrass.java
@@ -0,0 +1,53 @@
+package net.me.skyblock.block.custom;
+
+import net.me.skyblock.item.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.PlantBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+
+public class BlueVigilantGrass extends PlantBlock {
+
+    public static final BooleanProperty LIT = BooleanProperty.of("lit");
+
+    public BlueVigilantGrass(Settings settings) {
+        super(settings);
+    }
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(2.0, 0.0, 2.0, 14.0, 13.0, 14.0);
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player2, Hand hand, BlockHitResult hit) {
+
+        if(!world.isClient() && hand == Hand.MAIN_HAND) {
+            world.setBlockState(pos, state.cycle(LIT));
+        }
+
+        return super.onUse(state, world, pos, player2, hand, hit);
+    }
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(LIT);
+    }
+}
---
+++ src/main/java/net/me/skyblock/block/custom/YoungExoliusMaximus.java
@@ -0,0 +1,30 @@
+package net.me.skyblock.block.custom;
+
+import net.me.skyblock.block.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.PlantBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+
+public class YoungExoliusMaximus
+        extends PlantBlock {
+    protected static final float field_31235 = 6.0f;
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(2.0, 0.0, 2.0, 14.0, 13.0, 14.0);
+
+    public YoungExoliusMaximus(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+        return floor.isOf(ModBlocks.BLARU_MOSS) || floor.isOf(ModBlocks.VIGILANT_MOSS) || super.canPlantOnTop(floor, world, pos);
+    }
+}

0d4a64d71467c2d8514a472076fcfe3e39dd2840
StonksMan001 <toth.abel.2007@gmail.com>
2024-05-02 15:58:11 +0200

    v1.3.3 - updatelt Client registry-k

--- src/main/java/net/me/skyblock/SkyBlockClient.java
+++ src/main/java/net/me/skyblock/SkyBlockClient.java
@@ -3,18 +3,31 @@ package net.me.skyblock;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.me.skyblock.archived.ArchivedBlocks;
 import net.me.skyblock.blocks_and_items.ModBlocks;
 import net.me.skyblock.client.ModBlockEntityRendererFactories;
+import net.me.skyblock.client.ModScreenHandlers;
+import net.me.skyblock.client.gui.screens.AncientPedestalScreen;
 import net.me.skyblock.entity.ModEntities;
 import net.me.skyblock.entity.mobs.mcd.rendering.WispRenderer;
 import net.me.skyblock.api.ModModelPredicateProvider;
 import net.me.skyblock.entity.mobs.skyblock.rendering.OminousEvokerRenderer;
 import net.me.skyblock.particle.ModParticles;
 import net.me.skyblock.particle.halloween.NodeTracker;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.color.item.ItemColors;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.FoliageColors;
+import net.minecraft.client.color.world.GrassColors;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.EvokerEntityRenderer;
+import net.minecraft.item.BlockItem;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
 
 public class SkyBlockClient implements ClientModInitializer {
 
@@ -30,6 +43,8 @@ public class SkyBlockClient implements ClientModInitializer {
         // Should never touch
 
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SKYBLOCK__DUCKWEED, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SKYBLOCK__SMALL_LILY_PADS, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SKYBLOCK__CLOVER, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SKYBLOCK__SNOWY_SHORT_GRASS, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SKYBLOCK__ANT_JAM_BLOCK, RenderLayer.getTranslucent());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SKYBLOCK__SWEET_BERRY_JAM_BLOCK, RenderLayer.getTranslucent());
@@ -54,6 +69,8 @@ public class SkyBlockClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ABYSS__TESLA_FLOWER, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.DIGPEAR__CALLERY_LEAVES, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.DIGPEAR__FLOWERING_CALLERY_LEAVES, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SKYBLOCK__FIR_LEAVES, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SKYBLOCK__BLUEBERRY_BUSH, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.QUARK__CAVE_ROOTS, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SP5__END_GRASS_VEGETATION, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SP5__END_MUSHROOM, RenderLayer.getCutout());
@@ -69,6 +86,14 @@ public class SkyBlockClient implements ClientModInitializer {
         ModModelPredicateProvider.registerModModels();
         ModBlockEntityRendererFactories.registerModBlockEntityRendererFactories();
 
+        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getGrassColor(world, pos)
+                : GrassColors.getDefaultColor(), ModBlocks.SKYBLOCK__CLOVER);
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> GrassColors.getDefaultColor(), ModBlocks.SKYBLOCK__CLOVER);
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> GrassColors.getDefaultColor(), ModBlocks.SKYBLOCK__SMALL_LILY_PADS);
+        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> world != null && pos != null ? 2129968 : 7455580, ModBlocks.SKYBLOCK__SMALL_LILY_PADS);
+
         ParticleFactoryRegistry.getInstance().register(ModParticles.HALLOWEEN__NODE_TRACKER, NodeTracker.Factory::new);
+
+        HandledScreens.register(ModScreenHandlers.SKYBLOCK__ANCIENT_PEDESTAL_SCREEN_HANDLER, AncientPedestalScreen::new);
     }
 }

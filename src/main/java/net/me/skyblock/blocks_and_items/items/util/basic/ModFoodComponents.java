42bc4e207ed7c1a8f7b1ae0f4f5c7887d0ed36a1
StonksMan001 <toth.abel.2007@gmail.com>
2024-05-02 15:56:27 +0200

    v1.3.3 - Blueberry item FoodComponet registry

--- src/main/java/net/me/skyblock/blocks_and_items/items/util/basic/ModFoodComponents.java
+++ src/main/java/net/me/skyblock/blocks_and_items/items/util/basic/ModFoodComponents.java
@@ -3,10 +3,9 @@ package net.me.skyblock.blocks_and_items.items.util.basic;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
-import net.minecraft.item.FoodComponents;
 
 public class ModFoodComponents {
-    public static final FoodComponent ENDERITE_APPLE = new FoodComponent.Builder().hunger(4)
+    public static final FoodComponent SP5__ENDERITE_APPLE = new FoodComponent.Builder().hunger(4)
             .saturationModifier(1.2f)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 1200, 3), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 900, 2), 1.0f)
@@ -16,7 +15,7 @@ public class ModFoodComponents {
             .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 3200, 0), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 4400, 4), 1.0f)
             .alwaysEdible().build();
-    public static final FoodComponent ENCHANTED_ENDERITE_APPLE = new FoodComponent.Builder().hunger(4)
+    public static final FoodComponent SP5__ENCHANTED_ENDERITE_APPLE = new FoodComponent.Builder().hunger(4)
             .saturationModifier(1.2f)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 1600, 4), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 7000, 4), 1.0f)
@@ -32,7 +31,11 @@ public class ModFoodComponents {
             .statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 5000, 0), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.HERO_OF_THE_VILLAGE, 5000, 0), 1.0f)
             .alwaysEdible().build();
-    public static final FoodComponent STEAM_DUMPLING = new FoodComponent.Builder().hunger(5).saturationModifier(1.1f).build();
-    public static final FoodComponent STEAM_DUMPLING_WITH_MOLTEN_BUTTER = new FoodComponent.Builder().hunger(7).saturationModifier(1.4f).build();
-    public static final FoodComponent STEAM_DUMPLING_WITH_MOLTEN_BUTTER_AND_SUGAR_POPPY_MIXTURE = new FoodComponent.Builder().hunger(9).saturationModifier(1.9f).build();
+    public static final FoodComponent SKYBLOCK__STEAM_DUMPLING = new FoodComponent.Builder().hunger(5).saturationModifier(1.1f).build();
+    public static final FoodComponent SKYBLOCK__STEAM_DUMPLING_WITH_MOLTEN_BUTTER = new FoodComponent.Builder().hunger(7).saturationModifier(1.4f).build();
+    public static final FoodComponent SKYBLOCK__STEAM_DUMPLING_WITH_MOLTEN_BUTTER_AND_SUGAR_POPPY_MIXTURE = new FoodComponent.Builder().hunger(9).saturationModifier(1.9f).build();
+    public static final FoodComponent SKYBLOCK__BLUEBERRY = new FoodComponent.Builder().hunger(6).saturationModifier(2.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 2400, 4), 1.0f)
+            .alwaysEdible()
+            .build();
 }

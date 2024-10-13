842f0f35e6932331572ed6d768180dbf11257ddd
StonksMan001 <toth.abel.2007@gmail.com>
2024-10-13 14:27:48 +0200

    v1.4.1 - helytelen gamerule elnevezés fix

--- src/main/java/net/me/skyblock/server/ModGameRules.java
+++ src/main/java/net/me/skyblock/server/ModGameRules.java
@@ -9,7 +9,7 @@ public class ModGameRules {
     public static final GameRules.Key<GameRules.IntRule> SKYBLOCK__GOLDEN_CROP_GROWTH_CHANCE = GameRuleRegistry.register("skyblock__goldenCropGrowthChance", GameRules.Category.UPDATES, GameRuleFactory.createIntRule(25));
     public static final GameRules.Key<GameRules.IntRule> SKYBLOCK__RARE_MUSHROOM_GROWTH_CHANCE = GameRuleRegistry.register("skyblock__rareMushroomGrowthChance", GameRules.Category.UPDATES, GameRuleFactory.createIntRule(30));
     public static final GameRules.Key<GameRules.BooleanRule> SKYBLOCK__DO_ARROWS_BREAK_GLASS_BLOCKS = GameRuleRegistry.register("skyblock__doArrowsBreakGlassBlocks", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(true));
-    public static final GameRules.Key<GameRules.BooleanRule> MCD__POP_FLOWERS_ALWAYS_HIDE = GameRuleRegistry.register("mcd__pop_flowers_always_hide", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(false));
+    public static final GameRules.Key<GameRules.BooleanRule> MCD__POP_FLOWERS_ALWAYS_HIDE = GameRuleRegistry.register("mcd__popFlowersAlwaysHide", GameRules.Category.UPDATES, GameRuleFactory.createBooleanRule(false));
 
     public static void register() {
         SkyBlock.LOGGER.info("[SkyBlock MultiMod] Registering GameRules for " + SkyBlock.MOD_ID);

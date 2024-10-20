a8eaa41c7a9295822ad3b4f2c9ee76000ec0ab77
StonksMan001 <toth.abel.2007@gmail.com>
2024-10-20 14:39:26 +0200

    v1.4.1 - WrittenBookItem enchantment glint bugfix

--- src/main/java/net/me/skyblock/api/mixin/WrittenBookItemMixin.java
+++ src/main/java/net/me/skyblock/api/mixin/WrittenBookItemMixin.java
@@ -1,5 +1,6 @@
 package net.me.skyblock.api.mixin;
 
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.WrittenBookContentComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -11,22 +12,20 @@ import net.minecraft.util.StringHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
 @Mixin(WrittenBookItem.class)
-public abstract class WrittenBookItemMixin extends Item {
-    public WrittenBookItemMixin(Settings settings) {
-        super(settings);
-    }
-    @Override
-    public boolean hasGlint(ItemStack stack) {
-        return false;
+public abstract class WrittenBookItemMixin {
+    @ModifyVariable(method = "<init>", at = @At(value = "HEAD"), argsOnly = true)
+    private static Item.Settings removingEnchantmentGlintComponent(Item.Settings settings) {
+        return settings.component(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, null);
     }
     @Inject(method = "appendTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/StringHelper;isBlank(Ljava/lang/String;)Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
-    public void appendOriginalTitle(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type, CallbackInfo ci, WrittenBookContentComponent writtenBookContentComponent) {
+    private void appendOriginalTitle(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type, CallbackInfo ci, WrittenBookContentComponent writtenBookContentComponent) {
         if (!StringHelper.isBlank(writtenBookContentComponent.title().toString())) {
             tooltip.add(Text.translatable("book.original_title").formatted(Formatting.GRAY).append(Text.literal(writtenBookContentComponent.title().raw().formatted(Formatting.GRAY))));
         }

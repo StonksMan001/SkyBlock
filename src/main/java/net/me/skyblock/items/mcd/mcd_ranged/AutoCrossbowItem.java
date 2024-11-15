9a55b9b2d274b32787fef56e49bd31ca33b9d0af
StonksMan001 <toth.abel.2007@gmail.com>
2024-11-15 17:42:03 +0100

    v1.4.2 - AutoCrossbowItem behaviour + Accelerate enchantment

--- src/main/java/net/me/skyblock/items/mcd/mcd_ranged/AutoCrossbowItem.java
+++ src/main/java/net/me/skyblock/items/mcd/mcd_ranged/AutoCrossbowItem.java
@@ -1,20 +1,55 @@
 package net.me.skyblock.items.mcd.mcd_ranged;
 
+import net.me.skyblock.api.mixin.CrossbowItemAccessors;
 import net.me.skyblock.items.mcd.McdItem;
+import net.me.skyblock.registries.SkyBlock;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ChargedProjectilesComponent;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
 
 import java.util.List;
 
 public class AutoCrossbowItem extends CrossbowItem {
+    private static final float RELOAD_DECREASE_PERCENT = 0.09f; //12%
+    private static final float MIN_RELOAD_TIME = 0.25f; // 150% cap
+    private long lastShotTime = 0;
+    public float reloadBonus = 1.0f; // reload multiplier.
+
     public AutoCrossbowItem(Settings settings) {
         super(settings);
     }
 
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        long currentTime = world.getTime();
+        if (currentTime - lastShotTime > 100) reloadBonus = 1.0f;
+
+        ItemStack itemStack = user.getStackInHand(hand);
+        ChargedProjectilesComponent chargedProjectilesComponent = itemStack.get(DataComponentTypes.CHARGED_PROJECTILES);
+        if (chargedProjectilesComponent != null && !chargedProjectilesComponent.isEmpty()) {
+            this.shootAll(world, user, hand, itemStack, CrossbowItemAccessors.getSpeed(chargedProjectilesComponent), 1.0F, null);
+            lastShotTime = currentTime;
+            reloadBonus = Math.max(MIN_RELOAD_TIME, reloadBonus - RELOAD_DECREASE_PERCENT);
+            return TypedActionResult.consume(itemStack);
+        } else if (!user.getProjectileType(itemStack).isEmpty()) {
+            ((CrossbowItemAccessors) this).setCharged(false);
+            ((CrossbowItemAccessors) this).setLoaded(false);
+            user.setCurrentHand(hand);
+            return TypedActionResult.consume(itemStack);
+        } else {
+            return TypedActionResult.fail(itemStack);
+        }
+    }
     @Override
     public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
         tooltip.add(Text.translatable("tooltip.skyblock.minecraft_dungeons_header").setStyle(Style.EMPTY.withBold(true).withFormatting(Formatting.GRAY)));
